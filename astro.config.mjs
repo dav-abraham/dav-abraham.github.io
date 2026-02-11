02d80063444b51c2db3abd2f3c787fcd8d3c1c13
David Abraham <davabraham66@gmail.com>
2026-02-10 22:17:33 -0300

    fix: corrigindo sobreposição do menu via terminal

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,12 @@ import { defineConfig } from 'astro/config';
 import AutoImport from 'astro-auto-import';
 
 export default defineConfig({
+  // 1. Adicione a URL do seu novo domínio aqui
+  site: 'https://dav-abraham.github.io',
+  
+  // 2. Como o repositório tem o mesmo nome do usuário, a base é '/'
+  base: '/',
+
   integrations: [
     AutoImport({
       imports: [
